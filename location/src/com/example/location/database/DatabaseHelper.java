67432a300e27ee32b4659e95f5b494d22ea7697e
Shane1993 <513199010@qq.com>
2015-05-19 23:42:45 +0800

    Nothing

--- location/src/com/example/location/database/DatabaseHelper.java
+++ location/src/com/example/location/database/DatabaseHelper.java
@@ -17,14 +17,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 	
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		// TODO Auto-generated method stub
 		//这里创建两个表，一个用来存储区域定位信息，一个是存储个人行为定位信息的
 		//创建数据库的表名area（注意是表名，数据库的名称上面已经定了，叫location，这里这个表名是area，用来存放测量的区域信息的）
 			//后面一些是该表名的属性，有id（_id）,名称（name）和四个角落的经纬度
 		db.execSQL("create table area (id integer primary key, name varchar(10),"
 				+ "longitude1 decimal, longitude2 decimal, longitude3 decimal, longitude4 decimal, "
 				+ "latitude1 decimal, latitude2 decimal, latitude3 decimal, latitude4 decimal )");
-		//创建个人定位表名person，有id信息，经纬度信息，速度信息，时间信息
+		//创建个人定位表名person，有id信息，经纬度信息，速度信息，时间信息和区域名称
 		db.execSQL("create table person (id integer primary key, "
 				+ "longitude decimal, latitude decimal, speed decimal, time varchar(20),areaname varchar(20) )");
 	}
